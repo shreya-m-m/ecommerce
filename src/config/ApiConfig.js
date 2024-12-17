59d9f4feb5eb36b2ca47ccd11c87602ded66dc4d
shreya-m-m <shreya14102k@gmail.com>
2024-12-17 10:26:57 +0530

    change in api url

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// export const API_BASE_URL = "http://localhost:5454";
+
 export const API_BASE_URL = "https://ecommerce-backend-server.up.railway.app"
 
 const token= localStorage.getItem("token");
