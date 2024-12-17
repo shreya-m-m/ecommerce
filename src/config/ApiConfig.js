fa97f66343c2d8e91d14e98e670dce84a7229439
shreya-m-m <shreya14102k@gmail.com>
2024-12-17 10:13:33 +0530

    change in api

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5454";
-// export const API_BASE_URL = "https://shreya14102k.wixsite.com/trendista"
+// export const API_BASE_URL = "http://localhost:5454";
+export const API_BASE_URL = "https://ecommerce-backend-server.up.railway.app"
 
 const token= localStorage.getItem("token");
 
