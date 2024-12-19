3ebf7a008c39d2f3c0ed0b52212199fff3a9c7cf
shreya-m-m <shreya14102k@gmail.com>
2024-12-19 10:34:12 +0530

    updated

---
+++ src/config/ApiConfig copy.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+
+export const API_BASE_URL = "https://ecommerce-backend-server.up.railway.app"
+
+// export const API_BASE_URL = "http://localhost:5454"
+
+const token= localStorage.getItem("token");
+
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        Authorization: `Bearer ${token}`, // Get the token dynamically
+        "Content-Type": "application/json" // Set Content-Type to application/json
+    }
+});
+
+console.log("Api Response ", API_BASE_URL)
+
+export default api;
